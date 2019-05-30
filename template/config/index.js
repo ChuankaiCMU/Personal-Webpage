ee1f1ca630af749e0bb8e7265163f703a3d74a82
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2019-05-30 15:05:10 -0400

    Update public path

--- template/config/index.js
+++ template/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Personal-Webpage/',
     proxyTable: {},
 
     // Various Dev Server settings
