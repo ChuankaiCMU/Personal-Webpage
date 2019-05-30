f1f1bdc2b922eec5b7d555dba7a455ce7a4c611d
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2019-05-30 18:06:31 -0400

    Uploaded my image

--- docs/src/components/ProfileCard.vue
+++ docs/src/components/ProfileCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-card img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/me.jpg?raw=true" img-alt="Card image" img-left class="mb-3">
+        <b-card img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/me.jpg" img-alt="Card image" img-left class="mb-3">
       <b-card-text>
         Some quick example text to build on the card and make up the bulk of the card's content.
       </b-card-text>
