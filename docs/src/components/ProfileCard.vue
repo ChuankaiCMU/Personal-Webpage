85c65d624d18d5c9d9d813f30e7e798f872cc3de
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2019-06-01 13:27:34 -0400

    Added new library to adjust to different screen size

--- docs/src/components/ProfileCard.vue
+++ docs/src/components/ProfileCard.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <b-card class = "transparent" img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/me.jpg?raw=true" img-alt="Card image" img-left img-width = 36% img-height = 36%>
+<div>
+    <div v-if="$mq !== 'mobile'">
+        <b-card class = "transparent_profile" img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/me.jpg?raw=true" img-alt="Card image" img-left img-width = 36% img-height = 36%>
         <b-card-body>
       <b-card-text class = "textintro">
         Hi! I'm Charles. <br>
@@ -11,10 +12,23 @@
         </b-card-body>
     </b-card>
     </div>
+    <div v-if="$mq === 'mobile'">
+        <b-card class = "transparent_profile" img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/me.jpg?raw=true" img-alt="Card image" img-top img-width = 36% img-height = 36%>
+        <b-card-body>
+      <b-card-text class = "textintro">
+        Hi! I'm Charles. <br>
+         I'm a researcher, a designer, and a<br>
+        nonprofessional basketball player.<br>
+        Nice to meet you!
+      </b-card-text>
+        </b-card-body>
+    </b-card>
+    </div>
+</div>
 </template>
 
 <style>
-.transparent {
+.transparent_profile {
   background-color: transparent!important;
   margin-top: auto;
   margin-bottom: auto;
--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -5,9 +5,18 @@ import Construction from '@/components/Temppage'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueMq from 'vue-mq'
 
 Vue.use(Router)
 Vue.use(BootstrapVue)
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 450,
+    tablet: 900,
+    laptop: 1250,
+    desktop: Infinity
+  }
+})
 
 export default new Router({
   routes: [
