53b13a943d3bd7b9214a8b721fa1a1d83191b80a
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2019-07-10 16:02:08 -0500

    Uploaded pics

--- docs/src/components/Carousel.vue
+++ docs/src/components/Carousel.vue
@@ -21,16 +21,16 @@
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+      <b-carousel-slide img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/IMG_0492.PNG?raw=true">
         <h1>Hello world!</h1>
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+      <!-- <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide> -->
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
+      <!-- <b-carousel-slide>
         <img
           slot="img"
           class="d-block img-fluid w-100"
@@ -39,15 +39,15 @@
           src="https://picsum.photos/1024/480/?image=55"
           alt="image slot"
         >
-      </b-carousel-slide>
+      </b-carousel-slide> -->
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
           a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
         </p>
-      </b-carousel-slide>
+      </b-carousel-slide> -->
     </b-carousel>
 
     <p class="mt-4">
