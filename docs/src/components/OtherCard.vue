aad78e0d876aad5a24089de4814465f258c41e20
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2020-11-02 14:05:35 -0600

    Update OtherCard.vue

--- docs/src/components/OtherCard.vue
+++ docs/src/components/OtherCard.vue
@@ -26,7 +26,7 @@
     </b-card>
         <b-card title="Teaching Assistant" class = "transparent">
       <b-card-text >
-        2020 Fall  : CSCI 5115--User Interface Design, Implementation and Evaluation
+        2020 Fall  : CSCI 5115--User Interface Design, Implementation and Evaluation <br>
         2020 Spring: CSCI 5115--User Interface Design, Implementation and Evaluation
       </b-card-text>
     </b-card>
