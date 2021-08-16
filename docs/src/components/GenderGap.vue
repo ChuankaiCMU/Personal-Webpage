b47042ffe438936e2ece0857fa4525d891ddeefb
ChuankaiCMU <32274938+ChuankaiCMU@users.noreply.github.com>
2021-08-16 12:06:44 -0500

    Update components

---
+++ docs/src/components/GenderGap.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <div v-if="$mq !== 'mobile'">
+        <b-card class = "transparent_capstone" img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/gender.png?raw=true" img-alt="Card image" img-left img-width = 36% img-height = 36%>
+        <b-card-body>
+      <b-card-text class = "textcapstone">
+        <h4> Wikidata Gender Gap Case Study </h4>
+        A gender gap has been identified in Wikidata: Less than 22% of the human items are about women. We ask: What is the source of this gender gap? Is it a result from action of Wikidata editors or is it only reflecting the existing real world gender
+         bias? Through a case study, we compared data from Wikidata along with other two data sources. We found that: (i) The most popular categories of people in Wikidata are male-dominant professions such as American football. (ii) Within a certain set of
+        academic professions, Wikidata is no more biased than the real world notable assessments. Furthermore, the quantity and quality of men and women Wikidata items are similar.
+      <a href = "https://nbviewer.jupyter.org/github/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/publication/opensym2021-6.pdf" class = "paper">
+          [Paper (preprint) : In 17th International Symposium on Open Collaboration (OpenSym 2021)]
+        </a>
+      </b-card-text>
+        </b-card-body>
+    </b-card>
+    </div>
+    <div v-if="$mq === 'mobile'">
+        <b-card class = "transparent_capstone" img-src="https://github.com/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/assets/gender.png?raw=true" img-alt="Card image" img-top img-width = 100% >
+        <b-card-body>
+      <b-card-text class = "textcapstone">
+          <h4> Wikidata Gender Gap Case Study </h4>
+        A gender gap has been identified in Wikidata: Less than 22% of the human items are about women. We ask: What is the source of this gender gap? Is it a result from action of Wikidata editors or is it only reflecting the existing real world gender
+         bias? Through a case study, we compared data from Wikidata along with other two data sources. We found that: (i) The most popular categories of people in Wikidata are male-dominant professions such as American football. (ii) Within a certain set of
+        academic professions, Wikidata is no more biased than the real world notable assessments. Furthermore, the quantity and quality of men and women Wikidata items are similar.
+      <a href = "https://nbviewer.jupyter.org/github/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/publication/opensym2021-6.pdf" class = "paper">
+          [Paper (preprint) : In 17th International Symposium on Open Collaboration (OpenSym 2021)]
+        </a>
+      </b-card-text>
+        </b-card-body>
+    </b-card>
+    </div>
+</div>
+</template>
+
+<style>
+.transparent_capstone {
+  background-color: transparent!important;
+  margin-top: auto;
+  margin-bottom: auto;
+ }
+ .textcapstone{
+     color:black;
+     text-align: left!important;
+ }
+</style>
--- docs/src/components/Research.vue
+++ docs/src/components/Research.vue
@@ -4,6 +4,9 @@
     <Navbar></Navbar>
     </div>
     <div class = "capstone-container">
+    <Gender></Gender>
+    </div>
+    <div class = "capstone-container">
     <Capstone></Capstone>
     </div>
     <div class = "capstone-container">
@@ -11,6 +14,11 @@
     </div>
     <div class = "capstone-container">
     <b-card title="Publication" class = "transparent">
+      <b-card-text>
+        <a href = "https://nbviewer.jupyter.org/github/ChuankaiCMU/Personal-Webpage/blob/master/docs/src/publication/opensym2021-6.pdf" class = "paper">
+          Charles Chuankai Zhang and Loren Terveen. 2021. Quantifying the Gap: A Case Study of Wikidata Gender Disparities. In 17th International Symposium on Open Collaboration (OpenSym 2021)
+        </a>
+      </b-card-text>
       <b-card-text>
         <a href = "https://files.eric.ed.gov/fulltext/ED599222.pdf" class = "paper">
           Zhang, C., Huang, Y., Wang, J., Fang, W., Lu, D., Stamper, J., Fancsali, S., Holstein, K., & Aleven, V. (2019).  Early detection of wheel spinning: Comparison across tutors, models, features, and operationalizations. In Proceedings of the Twelfth International Conference on Educational Data Mining (EDM’19). IEDMS.
@@ -25,9 +33,11 @@
 import Navbar from './Navbar.vue'
 import Capstone from './Capstone.vue'
 import AbNet from './AbstractNet.vue'
+import Gender from './GenderGap.vue'
 export default {
   components: {
     Navbar,
+    Gender,
     Capstone,
     AbNet
   }
